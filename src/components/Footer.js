a70420066a36c24e17cccd4e7db1ff2c6052b1fe
j0nspa <loldexterity@gmail.com>
2022-10-10 19:04:49 +0300

    SPA example tehtävä

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer>
+        <div>
+            <span>Place sticky footer here</span>
+        </div>
+    </footer>
+  )
+}
